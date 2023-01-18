76a257d8ec6aae0a419f8532d2a6382c034a2982
SaraDonzellini <saradonzie@gmail.com>
2023-01-18 18:17:01 +0100

    component 'bakery special moments' implemented

--- src/components/main/BakerySpecialMoments.vue
+++ src/components/main/BakerySpecialMoments.vue
@@ -9,39 +9,74 @@ export default {
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <h6>
-            CORPORATE & WEDDINGS
-          </h6>
+          <div class="title_container">
+            <h6>
+              CORPORATE & WEDDINGS
+            </h6>
+            <h2>
+              Baking Special Moments
+            </h2>
+
+          </div>
         </div>
       </div>
     </div>
     <div class="container-fluid">
-      <div class="row d-flex">
+      <div class="row flex">
         <div class="col-6">
           <div class="box">
             <img src="../../assets/imgs/corporate-bg.jpg" alt="">
             <img src="../../assets/imgs/corporate-hover-bg.jpg" alt="" class="d-none">
-
           </div>
         </div>
-        <div class="col-6"></div>
+        <div class="col-6">
           <div class="box">
-            <img src="../../assets/imgs/corporate-bg.jpg" alt="">
-            <img src="../../assets/imgs/corporate-hover-bg.jpg" alt="" class="d-none">
+            <img src="../../assets/imgs/wedding-bg.jpg" alt="">
+            <img src="../../assets/imgs/wedding-hover-bg.jpg" alt="" class="d-none">
 
-          </div>
+        </div>
+
+        </div>
       </div>
     </div>
-    
+
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .box{
-    width: 40vw;
+@use '../../assets/styles/style.scss' as *;
+@use '../../assets/styles/partials/_variables.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss' as *;
 
-    img{
-      width:100%
-    }
+h6 {
+  font-size: small;
+}
+
+h2 {
+  font-weight: bold;
+  color: $daisy-bush;
+  margin-bottom: 3rem;
+}
+
+.flex {
+  display: flex;
+
+}
+
+.wrapper {
+  height: 100vh;
+}
+
+.title_container {
+  text-align: center;
+}
+
+.box {
+  width: 40vw;
+  margin: auto;
+
+  img {
+    width: 100%
   }
+}
 </style>
